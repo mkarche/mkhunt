4552e528c35283f12d22d098fcddad5c88f64403
mkarche <karchemohamed@gmail.com>
2021-05-23 21:13:48 +0100

    Generating controllers category & product with their respective index fuctions & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'product#index'
+  get '/categories', to: 'category#index', as: 'categories'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
